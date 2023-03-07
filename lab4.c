f7cf4690e2c897b8422f5bdceb25310de3f51ed5
yugashinde <123037363+yugashinde@users.noreply.github.com>
2023-03-07 00:32:50 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -4,23 +4,18 @@
 int main(int argc,char** argv){
         int sum = 0 ;
         int i = 0;
-        if (argc == 1){
+        if (argc < 4 || argc > 4){
                 return 0;
         }
-        else if (argc == 2){
-                return 0;
-        }
-        else if (argc == 3){
-                return 0;
-        }
-        else if ( argc == 4){
-               for (i=0; i <= argc ;i++){
-                        sum=sum + atoi(argv[i]);
+        else if (argc == 4 ){
+                for (i=0;i<argc;i++){
+                sum=sum + atoi(argv[i]);
                 }
                 return sum;
         }
-        else{
+        else {
                 return 0;
         }
+        
       
 }
