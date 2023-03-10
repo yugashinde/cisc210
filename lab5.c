947e9a69e23cf3796ac687c4d860387d1a87b981
yugashinde <123037363+yugashinde@users.noreply.github.com>
2023-03-10 15:36:56 -0500

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,67 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAX_NUMBER 100
 int main(int argc ,char** argv){
         char remainders[MAX_NUMBER];
         int number;
-        
+        int x = 0;
         scanf("%d",&number);
         int i = 1 ;
         while(number > 0 ){
                 remainders[i] = number % 2 ;
                 number = number / 2;
- 
                 i++;
         } 
-        int x = 0;
         for(x = i-1 ;x > 0;x--){
                 printf("%d",remainders[x]);
         }               
         return 0;
-}
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
-~                                                                                                                                                                                                           
--- INSERT --                                                                                                                                                                              18,2          All
+}                                                                                                                                                                              18,2          All
