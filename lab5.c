dd70fec3d24a2f716b305afa8db0e17d2df8d68a
yugashinde <123037363+yugashinde@users.noreply.github.com>
2023-03-10 15:46:04 -0500

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -4,15 +4,15 @@
 int main(int argc ,char** argv){
         char remainders[MAX_NUMBER];
         int number;
-        int x = 0;
+       
+        int i = 0;
         scanf("%d",&number);
-        int i = 1 ;
         while(number > 0 ){
                 remainders[i] = number % 2 ;
                 number = number / 2;
                 i++;
         } 
-        for(x = i-1 ;x > 0;x--){
+        for(int x = count - 1, x >= 0, x--){
                 printf("%d",remainders[x]);
         }               
         return 0;
