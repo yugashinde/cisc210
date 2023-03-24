43ea026131e532fe43f7366045913ad209da0cc3
yugashinde <123037363+yugashinde@users.noreply.github.com>
2023-03-23 20:05:02 -0400

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -4,9 +4,10 @@ int main(int argc,char** argv){
         printf("Enter two integers separated by a space\n");
         scanf("%d %d",&x,&y);
         calculate(x,y,&sum,&difference,&product);
-        printf("The sum of %d and %d is %dii\n",x,y,sum);
-        printf("The difference of %d and %d iiiirjgjnfjgnoGNncis %d\n",x,y,difference);
+        printf("The sum of %d and %d is %d\n",x,y,sum);
+        printf("The difference of %d and %d is %d\n",x,y,difference);
         printf("The product of %d and %d is %d\n",x,y,product);
+}
         
         
 }
